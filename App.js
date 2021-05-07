2c57da1fd1fbf974cf04cd201deeb63a5007b588
johnnie71 <johnnie.agonz71@gmail.com>
2021-05-07 12:29:01 -0400

    getting notifiation token from expo

--- App.js
+++ App.js
@@ -24,7 +24,9 @@ export default function App() {
         throw new Error('Permission not granted!');
       }
     })
-    .then(() => {})
+    .then(() => {
+      Notifications.getExpoPushTokenAsync();
+    })
     .catch((err) => {
       return null;
     });
